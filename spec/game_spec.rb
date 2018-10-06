b1a861a3fe304c7bc8b089550250a4f6c14d59ea
Lucien Najev <luciennajev@hotmail.com>
2018-10-06 18:31:59 +0100

    testing

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -5,6 +5,8 @@ describe Game do
   let(:mockComputer) { double :computer, :computer_choice => 'paper' }
   let(:mockPlayer) { double :player, choice: 'rock' }
   let(:game) { described_class.new(mockPlayer, mockComputer)}
+  # when passing in the mocks into the described class arguments you need to just use words and not symbols!!,
+
 
   describe '#decide_winner' do
     it "decides who wins the game" do
